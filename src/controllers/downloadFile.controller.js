488e696f960079dacdda127bc20b0781cfbdad0e
Riki <aguilcasariki@gmail.com>
2024-03-15 01:26:56 -0400

    Update downloadFile controller to accept file path and download it

--- src/controllers/downloadFile.controller.js
+++ src/controllers/downloadFile.controller.js
@@ -1,11 +1,23 @@
-export const downloadFile = (filepath, res) => {
-  return new Promise((resolve, reject) => {
-    res.download(filepath, (err) => {
-      if (err) {
-        reject(new Error("Error downloading file"));
-      } else {
-        resolve(true);
-      }
-    });
-  });
+import fs from "fs";
+import path from "path";
+export const downloadFile = (req, res) => {
+  const filePath = req.params.filePath;
+
+  try {
+    if (!fs.existsSync(filePath)) {
+      return res.status(404).send("File not found");
+    }
+
+    res.setHeader(
+      "Content-Disposition",
+      "attachment; filename=" + path.basename(filePath)
+    );
+    res.setHeader("Content-Type", "application/octet-stream");
+
+    const fileStream = fs.createReadStream(filePath);
+    fileStream.pipe(res);
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).send("Error al leer el archivo.");
+  }
 };
--- src/routes/download.route.js
+++ src/routes/download.route.js
@@ -1,26 +1,9 @@
 import { Router } from "express";
-import { fileURLToPath } from "url";
-import path from "path";
-import { downloadFile } from "../controllers/downloadFile.controller.js";
-import { fileExists } from "../controllers/fileExist.controller.js";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import { downloadFile } from "../controllers/downloadFile.controller.js";
 
 const downloadRouter = Router();
 
-downloadRouter.get("/download/:filename", async (req, res) => {
-  const filename = req.params.filename;
-
-  const filepath = path.join(__dirname, "../../upload", filename);
-
-  try {
-    await fileExists(filepath);
-    await downloadFile(filepath, res);
-  } catch (error) {
-    console.error(error.message);
-    return res.status(404).send("File not found");
-  }
-});
+downloadRouter.get("/download/:filePath(*)", downloadFile);
 
 export default downloadRouter;
